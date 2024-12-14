fb2956d93068b9aee3c8357521353da892f52b8c
Merve Safa özcan <mervesafa003@gmail.com>
2024-12-14 12:40:51 +0300

    Basit-sifreleme-program
    Python ile yazılmıştır.

---
+++ şifreleme programı .py
@@ -0,0 +1,18 @@
+uname = "Merve Safa"
+passwd = "h,sklDnlS."
+
+sayac = 3
+while sayac!=0:
+    sayac -=1
+    username = input("Kullanıcı adınızı girin : ")
+    if uname != username :
+        print(f"Kullanıcı adı yanlış , kalan deneme hakkınız {sayac}  ")
+    if uname == username:
+        password = input("Şirenizi girin : ")
+        if passwd == password:
+            print("Hoş geldiniz")
+        else:
+            print(f"Şifre Yanlış , kalan deneme hakkınız {sayac}")
+
+
+
